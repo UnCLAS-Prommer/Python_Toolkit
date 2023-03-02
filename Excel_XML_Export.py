30bf64f507b64ee4f336d48136c653eca08a2ce8
UnCLAS-Prommer <gitunclasprommer@gmail.com>
2023-03-03 00:00:57 +0800

    Upload files

--- Excel_XML_Export.py
+++ Excel_XML_Export.py
@@ -1,24 +1,31 @@
 import openpyxl
+import copy
 
 class Pos :
-    column = 0
-    row = 0
+    def __init__(self):
+        self.column = 0
+        self.row = 0
 class Cells :
-    start = Pos()
-    end = Pos()
     rowrange = 0
     columnrange = 0
+    def __init__(self):
+        self.start = Pos()
+        self.end = Pos()
 
 def get_merge_range(sheet):
     merged_ranges = []
-    mergecell = Cells()
     for i in sheet.merged_cells.ranges:
-        tmp = str(i).split(":")
-        mergecell.start.column = sheet[tmp[0]].column
-        mergecell.start.row = sheet[tmp[0]].row
-        mergecell.end.column = sheet[tmp[1]].column
-        mergecell.end.column.row = sheet[tmp[1]].row
+        mergecell = Cells()
+        mergecell.start.column = i.min_col
+        mergecell.start.row = i.min_row
+        mergecell.end.column = i.max_col
+        mergecell.end.row = i.max_row
+        mergecell.rowrange = i.size['rows']
+        mergecell.columnrange = i.size['columns']
         merged_ranges.append(mergecell)
+        del mergecell.start
+        del mergecell.end
+    print(merged_ranges)
     return merged_ranges
 
 
@@ -29,23 +36,28 @@ def Main():
     worksheet = document.active
     maxcols = 0
     cols = 0
-    output.write('<!DOCTYPE html>\n')
-    output.write("<table>\n")
+    output.write('<!DOCTYPE html>\n<html>\n<head></head>\n<body>\n')
+    output.write('<table border = "1">\n')
     merged_ranges = get_merge_range(worksheet)
     for row in worksheet.iter_rows():
-        output.write("<row>\n")
+        output.write("<tr>\n")
         cols = 0
         for cell in row:
+            print("Cell Row:", cell.row, "Cell Col:", cell.column)
+            written = False
             for mergecell in merged_ranges:
+                print("MergeCell Row:", mergecell.start.row, "MergeCell Col:", mergecell.start.column)
                 if cell.column >= mergecell.start.column and cell.row >= mergecell.start.row and cell.row <= mergecell.end.row and cell.column <= mergecell.end.column:
-                    if cell.column == mergecell.start.column and cell.row == mergecell.start.column:
-                        output.write("<cells></cells>")
-                else:
-                    output.write("<cells>" + str(cell.value) + "</cells>\n")
+                    if cell.column == mergecell.start.column and cell.row == mergecell.start.row:
+                        output.write('<td rowspan = "' + str(mergecell.rowrange) + '" colspan = "' + str(mergecell.columnrange) + '">' + str(cell.value) + '</td>\n')
+                        written = True
+            if not written:
+                output.write("<td>" + str(cell.value) + "</td>\n")
             cols += 1
         if cols > maxcols:
             maxcols = cols
-        output.write("</row>\n")
-    output.write("<maxcols>" + str(maxcols) + "</maxcols>")
-    output.write("</table>")
+        output.write("</tr>\n")
+    output.write("<maxcols>" + str(maxcols) + "</maxcols>\n")
+    output.write("</table>\n</body>\n</html>")
+
 Main()
