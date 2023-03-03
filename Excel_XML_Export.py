a53fe353ea10fef757d41765353edf14e3b8f065
UnCLAS-Prommer <gitunclasprommer@gmail.com>
2023-03-03 17:47:27 +0800

    Finish

--- Excel_XML_Export.py
+++ Excel_XML_Export.py
@@ -25,7 +25,6 @@ def get_merge_range(sheet):
     return merged_ranges
 
 filepath = input("File Path: ")
-output = open("output.html", "w",encoding = "utf8")
 filename = "Unknown"
 if filepath.find("\\") != -1:
     filename = filepath.split("\\")[-1]
@@ -33,17 +32,19 @@ elif filepath.find("/") != -1:
     filename = filepath.split("/")[-1]
 else:
     filename = filepath
+output = open(filename + ".html", "w",encoding = "utf8")
 document = openpyxl.load_workbook(filepath,data_only=True)
 worksheet = document.active
-maxcols = 0
-cols = 0
+ChangeFlag = False
+# maxcols = 0
+# cols = 0
 output.write('<!DOCTYPE html>\n<html>\n<head>\n')
-output.write('<style type="text/css">\ntable {table-layout: fixed;width: 100%;border-collapse: collapse;border: 3px solid purple;text-align: center;}\nth, td {padding: 2px;}\ncaption{font-weight: bold;}\n</style>\n')
-output.write('</head>\n<body>\n')
-output.write('<table border="2">\n<caption>' + filename + '</caption>')
+output.write('<style type="text/css">\ntable {table-layout: fixed;border-collapse: collapse;border: 5px solid grey;text-align: center;}\nth, td {padding: 2px;}\ncaption{font-weight: bold;}\ntd{border: 2px grey solid}\n.y{background-color: rgb(255, 232, 156)}\n.g{background-color: rgb(195, 255, 242)}\n</style>\n')
+output.write('</head>\n<body>\n<table border>\n<caption>' + filename + '</caption>')
 merged_ranges = get_merge_range(worksheet)
 for row in worksheet.iter_rows():
-    output.write("<tr>\n")
+    output_string = "<tr>\n"
+    # output.write("<tr>\n")
     cols = 0
     for cell in row:
         written = False
@@ -51,11 +52,20 @@ for row in worksheet.iter_rows():
             if cell.column >= mergecell.start.column and cell.row >= mergecell.start.row and cell.row <= mergecell.end.row and cell.column <= mergecell.end.column:
                 written = True
                 if cell.column == mergecell.start.column and cell.row == mergecell.start.row:
-                    output.write('<td rowspan = "' + str(mergecell.rowrange) + '" colspan = "' + str(mergecell.columnrange) + '">' + str(cell.value) + '</td>\n')
+                    if ChangeFlag:
+                        output_string += '<td class="y" rowspan = "' + str(mergecell.rowrange) + '" colspan = "' + str(mergecell.columnrange) + '">' + str(cell.value) + '</td>\n'
+                    else:
+                        output_string += '<td class="g" rowspan = "' + str(mergecell.rowrange) + '" colspan = "' + str(mergecell.columnrange) + '">' + str(cell.value) + '</td>\n'
+                    # output.write('<td rowspan = "' + str(mergecell.rowrange) + '" colspan = "' + str(mergecell.columnrange) + '">' + str(cell.value) + '</td>\n')
         if not written:
-            output.write("<td>" + str(cell.value) + "</td>\n")
+            if ChangeFlag:
+                output_string += '<td class="y">' + str(cell.value) + '</td>\n'
+            else:
+                output_string += '<td class="g">' + str(cell.value) + '</td>\n'
+            # output.write("<td>" + str(cell.value) + "</td>\n")
         cols += 1
-    if cols > maxcols:
-        maxcols = cols
-    output.write("</tr>\n")
+    # if cols > maxcols:
+    #     maxcols = cols
+    output.write(output_string + '</tr>\n')
+    ChangeFlag = not ChangeFlag
 output.write("</table>\n</body>\n</html>")
