11be4b669a72acd0420552de2b8ce667e1a950fc
UnCLASPrommer <gitunclasprommer@gmail.com>
2023-03-03 14:39:11 +0800

    Finish the main part

--- Excel_XML_Export.py
+++ Excel_XML_Export.py
@@ -6,11 +6,11 @@ class Pos :
         self.column = 0
         self.row = 0
 class Cells :
-    rowrange = 0
-    columnrange = 0
     def __init__(self):
         self.start = Pos()
         self.end = Pos()
+        rowrange = 0
+        columnrange = 0
 
 def get_merge_range(sheet):
     merged_ranges = []
@@ -23,41 +23,40 @@ def get_merge_range(sheet):
         mergecell.rowrange = i.size['rows']
         mergecell.columnrange = i.size['columns']
         merged_ranges.append(mergecell)
-        del mergecell.start
-        del mergecell.end
-    print(merged_ranges)
     return merged_ranges
 
-
-def Main():
-    filepath = input("File Path: ")
-    output = open("output.html", "w",encoding = "utf8")
-    document = openpyxl.load_workbook(filepath,data_only=True)
-    worksheet = document.active
-    maxcols = 0
+filepath = input("File Path: ")
+output = open("output.html", "w",encoding = "utf8")
+filename = "Unknown"
+if filepath.find("\\") != -1:
+    filename = filepath.split("\\")[-1]
+elif filepath.find("/") != -1:
+    filename = filepath.split("/")[-1]
+else:
+    filename = filepath
+document = openpyxl.load_workbook(filepath,data_only=True)
+worksheet = document.active
+maxcols = 0
+cols = 0
+output.write('<!DOCTYPE html>\n<html>\n<head>\n')
+output.write('<style type="text/css">\ntable {table-layout: fixed;width: 100%;border-collapse: collapse;border: 3px solid purple;text-align: center;}\nth, td {padding: 2px;}\ncaption{font-weight: bold;}\n</style>\n')
+output.write('</head>\n<body>\n')
+output.write('<table border="2">\n<caption>' + filename + '</caption>')
+merged_ranges = get_merge_range(worksheet)
+for row in worksheet.iter_rows():
+    output.write("<tr>\n")
     cols = 0
-    output.write('<!DOCTYPE html>\n<html>\n<head></head>\n<body>\n')
-    output.write('<table border = "1">\n')
-    merged_ranges = get_merge_range(worksheet)
-    for row in worksheet.iter_rows():
-        output.write("<tr>\n")
-        cols = 0
-        for cell in row:
-            print("Cell Row:", cell.row, "Cell Col:", cell.column)
-            written = False
-            for mergecell in merged_ranges:
-                print("MergeCell Row:", mergecell.start.row, "MergeCell Col:", mergecell.start.column)
-                if cell.column >= mergecell.start.column and cell.row >= mergecell.start.row and cell.row <= mergecell.end.row and cell.column <= mergecell.end.column:
-                    if cell.column == mergecell.start.column and cell.row == mergecell.start.row:
-                        output.write('<td rowspan = "' + str(mergecell.rowrange) + '" colspan = "' + str(mergecell.columnrange) + '">' + str(cell.value) + '</td>\n')
-                        written = True
-            if not written:
-                output.write("<td>" + str(cell.value) + "</td>\n")
-            cols += 1
-        if cols > maxcols:
-            maxcols = cols
-        output.write("</tr>\n")
-    output.write("<maxcols>" + str(maxcols) + "</maxcols>\n")
-    output.write("</table>\n</body>\n</html>")
-
-Main()
+    for cell in row:
+        written = False
+        for mergecell in merged_ranges:
+            if cell.column >= mergecell.start.column and cell.row >= mergecell.start.row and cell.row <= mergecell.end.row and cell.column <= mergecell.end.column:
+                written = True
+                if cell.column == mergecell.start.column and cell.row == mergecell.start.row:
+                    output.write('<td rowspan = "' + str(mergecell.rowrange) + '" colspan = "' + str(mergecell.columnrange) + '">' + str(cell.value) + '</td>\n')
+        if not written:
+            output.write("<td>" + str(cell.value) + "</td>\n")
+        cols += 1
+    if cols > maxcols:
+        maxcols = cols
+    output.write("</tr>\n")
+output.write("</table>\n</body>\n</html>")
