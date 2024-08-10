e9316662e30b196fc1b009130cf10e48096cc002
UnCLAS-Prommer <gitunclasprommer@gmail.com>
2024-08-10 13:38:22 +0800

    Modify Exif_Checker

--- Exif_Checker.py
+++ Exif_Checker.py
@@ -4,11 +4,17 @@ def OutputFileTime(BaseFilePath):
     FileList = os.listdir(BaseFilePath)
     for i in FileList:
         try:
-            img = Image.open(i)
+            img = Image.open(Path + "\\" + i)
             exif_dict = piexif.load(img.info['exif'])
             print(i + " Time: " + str(exif_dict['0th'][piexif.ImageIFD.DateTime]))
         except Exception as e:
             print(e)
 
-BaseFilePath = sys.argv[0]
-OutputFileTime(BaseFilePath)
+BaseFilePath = sys.argv[1]
+Path = ""
+for i in BaseFilePath:
+    if(i == '\\'):
+        Path += "\\\\"
+    else:
+        Path += i
+OutputFileTime(Path)
