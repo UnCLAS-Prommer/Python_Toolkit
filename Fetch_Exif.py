b190c9219eeb762da7fe38c6fac2ada77937370c
UnCLAS-Prommer <gitunclasprommer@gmail.com>
2024-08-12 00:44:56 +0800

    fix some bugs

--- Fetch_Exif.py
+++ Fetch_Exif.py
@@ -1,16 +1,28 @@
 import os, sys
 from PIL import Image
 from pillow_heif import register_heif_opener
+import piexif
+def GetExifData(i, img):
+    if(img.format == 'HEIF'):
+        exif_dict = img.getexif()
+        if (306 in exif_dict):
+            print(i + " Time: " + exif_dict[306])
+        else:
+            print(i + " have no time info.")
+    else:
+        exif_dict = piexif.load(img.info["exif"])
+        print(exif_dict['Exif'])
+        if(36867 in exif_dict['Exif']):
+            print(i + " Time: " + exif_dict['Exif'][36867])
+        else:
+            print(i + " have no time info.")
 def OutputFileTime(BaseFilePath):
     FileList = os.listdir(BaseFilePath)
     for i in FileList:
         try:
             img = Image.open(BaseFilePath + "\\" + i)
-            exif_dict = img.getexif()
-            if (306 in exif_dict):
-                print(i + " Time: " + exif_dict[306])
-            else:
-                print(i + " have no time info.")
+            GetExifData(i, img)
+            img.close()
         except Exception as e:
             print(e)
 register_heif_opener()
