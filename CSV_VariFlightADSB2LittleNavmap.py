9ae5e9c5890c9103cf53d8a929980876f68a82c7
UnCLAS-Prommer <gitunclasprommer@gmail.com>
2024-11-02 13:46:27 +0800

    Add a new tool

---
+++ CSV_VariFlightADSB2LittleNavmap.py
@@ -0,0 +1,24 @@
+import pandas, sys
+InputCSV = sys.argv[1]
+data = pandas.read_csv(InputCSV)
+IndentNum = 1
+LatCol = data['Latitude']
+LonCol = data['Longitude']
+IdnCol = []
+EmptyCol = []
+TypeCol = []
+for line in LatCol:
+    IdnCol.append(IndentNum)
+    EmptyCol.append('')
+    TypeCol.append('Pin')
+    IndentNum += 1
+with open('Output.csv', "w", encoding='utf-8') as f:
+    Frame = pandas.DataFrame({
+        'Type' : TypeCol,
+        'Empty2' : EmptyCol,
+        'Ident' : IdnCol,
+        'Lat' : LatCol,
+        'Lon' : LonCol,
+    })
+    Frame.to_csv(f, index=False, header=False)
+    f.close()
